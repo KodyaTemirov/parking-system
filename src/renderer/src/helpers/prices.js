6eb3ad10045d7a58f91cfc549d1746afc83b531d
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2025-03-25 12:55:33 +0500

    refactor: обновлены значения и цены для различных типов транспортных средств в файлах prices.js и helpers/prices.js

--- src/renderer/src/helpers/prices.js
+++ src/renderer/src/helpers/prices.js
@@ -1,23 +1,45 @@
 export const pricesData = [
   {
-    value: "Легковой автомобиль",
-    price: 20000,
+    value: "Yengil avtomashinalar va mikroavtobuslar",
+    price: 30000,
     id: 1,
     state: true,
     icon: "mdi:car",
   },
   {
-    value: "Грузовой автомобиль",
-    price: 30000,
+    value:
+      "Yuk avtomashinalari, avtobuslar, traktorlar va boshqa yuk ko‘taruvchi transport vositalari ",
+    price: 35000,
     id: 2,
     state: false,
     icon: "mdi:dump-truck",
   },
   {
-    value: "Мотоцикл или скутер",
-    price: 10000,
+    value: "Avtomobillarga qo‘shimcha ulov (pritsep)lar sotuvchilaridan ",
+    price: 15000,
     id: 3,
     state: false,
+    icon: "clarity:trailer-solid",
+  },
+  {
+    value: "MDH (SNG) davlatlarida ishlab chiqarilgan avtomobil sotuvchilaridan",
+    price: 22000,
+    id: 4,
+    state: false,
+    icon: "mdi:car",
+  },
+  {
+    value: "Mototsikl, motoroller va mopedlar",
+    price: 11000,
+    id: 5,
+    state: false,
     icon: "mdi:motorbike",
+  },
+  {
+    value: "Velosipedlar",
+    price: 9000,
+    id: 6,
+    state: false,
+    icon: "mdi:bike",
   },
 ];
